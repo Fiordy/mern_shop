0cc01bf5c2098c01bdfced5c896c7b2eba910cfb
Fiordy <fiordes.agaj@gmail.com>
2022-07-31 18:04:55 +0200

    things

--- backend/server.js
+++ backend/server.js
@@ -1,33 +1,45 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import colors from 'colors'
-import connectDB from './config/db.js'
-import products from './data/products.js'
+import express from "express";
+import dotenv from "dotenv";
+import colors from "colors";
+import connectDB from "./config/db.js";
 
-dotenv.config()
+import productRoutes from "./routes/productRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
-connectDB()
+dotenv.config();
+connectDB();
+const app = express();
 
-const app = express()
+/*-----------------------------------------------------------------*/
+/* Home base                                                       */
+/*-----------------------------------------------------------------*/
+app.get("/", (req, res) => {
+  res.send("API is running....");
+});
 
-app.get('/', (req, res) => {
-  res.send('API is running....')
-})
 
-app.get('/api/products', (req, res) => {
-  res.json(products)
-})
+/*-----------------------------------------------------------------*/
+/* Route registering                                               */
+/*-----------------------------------------------------------------*/
+app.use("/api/products", productRoutes);
 
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id)
-  res.json(product)
-})
 
-const PORT = process.env.PORT || 5000
+/*-----------------------------------------------------------------*/
+/* Middleware registering                                          */
+/*-----------------------------------------------------------------*/
+app.use(notFound)
+
+app.use(errorHandler)
+
+
+/*-----------------------------------------------------------------*/
+/* Server launch                                                   */
+/*-----------------------------------------------------------------*/
+const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT,
   console.log(
     `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
   )
-)
+);
